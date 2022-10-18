fdc9136ebb07b554ecca90a6f5039720a196269b
kiranbakale <you@example.com>
2022-10-18 11:01:41 +0530

    modified index file

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 
+<!DOCTYPE html>
 <html lang="en">
-    <h1>Added webhook</h1>
 <head>
-<title> PHP Example</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-CALCULATOR-APP</title>
 </head>
 <body>
-<h1>hello there</h1>
-<div>Next, we’ll display today’s date and day by PHP!</div>
+<div> we’ll display today’s date and day by PHP!</div>
 <div>
 <?php
 echo 'Today’s date is <b>' . date('Y/m/d') . '</b> and it’s a <b>'.date('l').'</b> today!';
 ?>
 </div>
-<div>Again, this is static HTML content.</div>
+
 </body>
 </html>
